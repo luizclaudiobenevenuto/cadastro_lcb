801634a81d8de7567e506929df39419a282987fb
luiz cladio <luizcladiobenevenutomota@gmail.com>
2022-04-12 11:42:13 -0300

    Salvando dados no banco de dados MySQL

--- cadastro/settings.py
+++ cadastro/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Configuração de e-mail
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
