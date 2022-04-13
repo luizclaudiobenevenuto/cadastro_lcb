3841f5aea22f8dfd354b9abd259d6bb7b5bca0c4
luiz cladio <luizcladiobenevenutomota@gmail.com>
2022-04-13 10:24:39 -0300

    Seção de usuários

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
 from django.contrib import messages
-
+from django.shortcuts import redirect
 from .forms import ContaForm, ProdutoModelForm
 from .models import Produto
 
@@ -28,19 +27,22 @@ def contato(request):
     return render(request, 'contato.html', context)
 
 def produto(request):
-    if str(request.method) == 'POST':
-        form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
 
-            form.save()
+                form.save()
 
-            messages.success(request, 'Produto salvo com sucesso.')
-            form = ProdutoModelForm()
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar o produto. ')
         else:
-            messages.error(request, 'Erro ao salvar o produto. ')
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
     else:
-        form = ProdutoModelForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'produto.html', context)
+        return redirect('index')
